53b41ab91e21c91a771bde8437b04fa1a30e0946
Catarino Grosser Ferreira <87507933+catarino-grosser@users.noreply.github.com>
2021-12-09 09:45:57 -0300

    Add files via upload

---
+++ garopabahosthouse/scripts/openPage.js
@@ -0,0 +1,54 @@
+// open Page 
+    // open page A e botao A
+function openPageA() {
+    openPageAfx()
+    openBotaoVoltarA()
+}
+function openBotaoVoltarA() {
+    document.getElementById("botaoVoltarA").style.display="block"
+}
+function openPageAfx() {
+    document.getElementById("page-A").style.height="100vh";
+}
+
+    // open page B e botao B
+
+    function openPageB() {
+        openPageBfx()
+        openBotaoVoltarB()
+}
+function openPageBfx() {
+    document.getElementById("page-B").style.height="100vh";
+}
+function openBotaoVoltarB() {
+    document.getElementById("botaoVoltarB").style.display="block"
+}
+
+
+function openPageC() {
+    openPageCfx()
+    openBotaoVoltarC()
+}
+
+function openPageCfx() {
+    document.getElementById("page-C").style.height="100vh";
+}
+function openBotaoVoltarC() {
+    document.getElementById("botaoVoltarC").style.display="block"
+}
+
+
+
+function openPageD() {
+    openPageDfx()
+    openBotaoVoltarD()
+}
+function openBotaoVoltarD() {
+    document.getElementById("botaoVoltarD").style.display="block"
+}
+function openPageDfx() {
+    document.getElementById("page-D").style.height="100vh";
+}
+
+
+
---
+++ garopabahosthouse/scripts/openSubPageC.js
@@ -0,0 +1,16 @@
+// openSubPage C
+function openSubPageC1() {
+    document.getElementById("subPageC-1").style.height="100vh";
+}
+function openSubPageC2() {
+    document.getElementById("subPageC-2").style.height="100vh";
+}
+function openSubPageC3() {
+    document.getElementById("subPageC-3").style.height="100vh";
+}
+function openSubPageC4() {
+    document.getElementById("subPageC-4").style.height="100vh";
+}
+function openSubPageC5() {
+    document.getElementById("subPageC-5").style.height="100vh";
+}
