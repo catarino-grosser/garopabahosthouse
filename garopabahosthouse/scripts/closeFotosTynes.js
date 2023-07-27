92f5cc6cd841a066bb4dc35cdca1a91b24765629
Catarino Grosser Ferreira <87507933+catarino-grosser@users.noreply.github.com>
2023-07-27 11:47:40 -0300

    Delete garopabahosthouse directory

--- garopabahosthouse/scripts/closeFotosTynes.js
+++
@@ -1,9 +0,0 @@
-function closeFoto01B1() {
-    document.getElementById("foto01B1").style.height="0";
-}
-function closeFoto02B1() {
-    document.getElementById("foto02B1").style.height="0";
-}
-function closeFoto03B1() {
-    document.getElementById("foto03B1").style.height="0";
-}
